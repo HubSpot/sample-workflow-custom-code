79e1cf2d0a6360c4a3192572c64d3ae80e598b48
janbogaert <103489502+janbogaert@users.noreply.github.com>
2022-05-11 13:45:55 +0200

    Create codedAction.js

---
+++ Use Cases/codedAction.js
@@ -0,0 +1,19 @@
+
+   
+/**
+ * For a detailed write up on the use cases for this snippet, see
+ * https://docs.google.com/document/d/15WA0YBX29ZUE3tdUCu7VnnXaC3A2OaIy4iv7I-snytE/edit?usp=sharing
+ */
+const hubspot = require('@hubspot/api-client')
+
+exports.main = (event) => {
+  const hubspotClient = new hubspot.Client({
+    apiKey: process.env.HAPIKEY
+  });
+
+  // Archive a contact by ID
+  hubspotClient.crm.contacts.basicApi.archive(event.object.objectId)
+    .then(resp => {
+      console.log('Contact archived')
+    });
+};
