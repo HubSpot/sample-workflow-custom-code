17efeba185871e87e79b0533c4cec186b4e612d2
Jan Bogaert <103489502+janbogaert@users.noreply.github.com>
2022-07-12 10:49:30 +0200

    Create linebreak.js

---
+++ samples/linebreak.js
@@ -0,0 +1,24 @@
+const hubspot = require('@hubspot/api-client');
+
+exports.main = (event, callback) => {
+  const hubspotClient = new hubspot.Client({
+    apiKey: process.env.HAPIKEY
+  });
+
+  // replace 'multiline' with your multi line text property name
+  hubspotClient.crm.contacts.basicApi.getById(event.object.objectId, ['multiline'])
+    .then(results => {
+      let multiline = results.body.properties.multiline;
+    console.log(multiline);
+	  multiline = multiline.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    console.log(multiline);
+    
+      callback({
+        outputFields: {
+          multiline
+        }
+      });
+    })
+}
+
+// Orignal author: Tyler Gunji (https://gist.github.com/dshukertjr)
